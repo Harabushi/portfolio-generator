7cd4e0cd6e47811ad56a2eda2d1de164af172d78
Colby Harris <harris.colby@gmail.com>
2022-02-15 18:06:02 -0600

    added inquirer and started setup to get user information using inquirer

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-// These two can be replaced with the one above
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
-
-  console.log('Portfolio Complete! Check out index.html to see the output!')
-});
+// const pageHTML = generatePage(name, github);
 
 
-/*
-* notes on the arrow function
-*/
+// fs.writeFile('index.html', generatePage(name, github), err => {
+//   if (err) throw err;
 
-// In arrow functions, paranthesis are not required when there is exactly one parameter, for 0 or 2 or above we need them
-// likewise, curly braces and the rturn call aren't needed with only one statement inside
+//   console.log('Portfolio Complete! Check out index.html to see the output!')
+// });
 
-// // Notice the lack of parentheses around the `profileDataArr` parameter?
-// const printProfileData = profileDataArr => {
-//   // This...
-//   for (let i = 0; i < profileDataArr.length; i += 1) {
-//     console.log(profileDataArr[i]);
-//   }
-
-//   console.log('================');
-
-//   // Is the same as this...
-//   profileDataArr.forEach((profileItem) => {
-//     console.log(profileItem)
-//   });
-
-//   console.log('================');
-
-//   // And this... but only for functions with a single task inside of it
-//   profileDataArr.forEach(profileItem => console.log(profileItem));
-// };
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
